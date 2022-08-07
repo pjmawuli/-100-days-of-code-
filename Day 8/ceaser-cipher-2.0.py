4b3f3ddd330c6f1681fc9a2b116e7aae92ccf14c
pjmawuli <87507466+pjmawuli@users.noreply.github.com>
2022-08-07 21:55:45 +0000

    Update ceaser-cipher 2.0.py

--- Day 8/ceaser-cipher-2.0.py
+++ Day 8/ceaser-cipher-2.0.py
@@ -5,7 +5,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 # direction = input("Type 'encode' to encrypt, type 'decode' to decode:\n"
 print(art.logo)
-direction = input("Do you want to decode or encrypt? \n")
+direction = input("Do you want to decode or encode? \n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -19,6 +19,11 @@ def ceaser(text, shift, direction):
         position = alphabet.index(char)
         new_position = position
         new_position += shift
+
+        if direction.lower() == "decode":
+            new_position %= -26
+        elif direction.lower() == "encode":
+            new_position %= 26
         new_text += alphabet[new_position]
     print(new_text)
 
